ab6d27d4d574e8ad24f1fad045106ce710ff060b
Manoj Sirsath <sirsathmanoj5@gmail.com>
2025-01-04 18:30:09 +0530

    Added two program find the first occurance in staring and length of last word

---
+++ dist/28-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,23 @@
+"use strict";
+function findOccuranceInString(haystack, needle) {
+    const M = needle.length;
+    const N = haystack.length;
+    if (N === 1 && M === 1) {
+        if (needle[M] === haystack[N])
+            return 0;
+    }
+    //sliding window approach
+    for (let windowStart = 0; windowStart < N - M; windowStart++) {
+        for (let i = 0; i < M; i++) {
+            if (needle[i] !== haystack[windowStart + i]) {
+                break;
+            }
+            if (i === M - 1) {
+                return windowStart;
+            }
+        }
+    }
+    return -1;
+}
+const haystack = "a", needle = "a";
+console.log(findOccuranceInString(haystack, needle));
---
+++ dist/58-length-of-last-word.js
@@ -0,0 +1,41 @@
+"use strict";
+function lengthOfLastWord(s) {
+    // My Approach - I
+    // const splittedString = s.split(' ');
+    // let len = -1;
+    // for(let i = splittedString.length-1; i>=0; i--) {
+    //     if(splittedString[i] !== '') {
+    //         len =splittedString[i].length
+    //         break;
+    //     }
+    // }
+    // return len;
+    ////Approach - II
+    // let strIndex = s.length - 1;
+    // while(strIndex >=0 && s[strIndex] === " ") {
+    //     strIndex--;
+    // }
+    // let lastWordLength = 0;
+    // while(strIndex >=0 && s[strIndex] !== " ") {
+    //     strIndex--;
+    //     lastWordLength++;
+    // }
+    // return lastWordLength;
+    //// Approach - III
+    // let strIndex = s.length;
+    // let lastWordLength = 0;
+    // while(strIndex > 0) {
+    //     strIndex--;
+    //     if(s[strIndex] !== " ") {
+    //         lastWordLength++;
+    //     } else if(lastWordLength > 0) {
+    //         return lastWordLength;
+    //     }
+    // }
+    // return lastWordLength;
+    //Approach with built in function - IV
+    let str = s.trim();
+    return s.length - s.lastIndexOf(" ") - 1;
+}
+const str = "luffy is still joyboy";
+console.log(lengthOfLastWord(str));
